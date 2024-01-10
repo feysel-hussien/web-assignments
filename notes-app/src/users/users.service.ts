fa34d1cfa7eae75dead63a1be71fc9c8b616aee6
Nahom Garefo <harobina28@gmail.com>
2024-01-10 10:41:02 +0300

    intial step for JWT authentication

--- notes-app/src/users/users.service.ts
+++ notes-app/src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
+export type User =any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users=[
+        {
+            userId:1,
+            username:'nahom',
+            password:'123'
+        },
+        {
+            userId:2,
+            username:'mikiyas',
+            password:'123'
+        },
+        {
+            userId:3,
+            username:'lemi',
+            password:'123'
+        },
+
+    ];
+    async findOne(username:string): Promise<User | undefined>{
+        return this.users.find(user=> user.username ===username);
+    }
+}
