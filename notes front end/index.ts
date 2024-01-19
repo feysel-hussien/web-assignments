3b1c131136ab9b03993143d42551d66ec251ab1e
Nahom Garefo <harobina28@gmail.com>
2024-01-19 16:45:55 +0300

    front end configuration

---
+++ notes front end/index.ts
@@ -0,0 +1,22 @@
+document.getElementById('loginButton')?.addEventListener('click', async function(event) {
+    event.preventDefault();
+  
+    const email = (document.getElementById('useremail') as HTMLInputElement)?.value;
+    const password = (document.getElementById('userpassword') as HTMLInputElement)?.value;
+
+        const response =  await fetch('http://localhost:5001/users/login',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'},
+            body:JSON.stringify({email,password})
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            window.location.href = 'user/userindex.html';
+        } else {
+            console.log('error', response.status, response.statusText);
+        }
+
+});
